8705639cc905b447a1af841524ebcc95f8a2105b
alex <ibymif2000@yandex.ru>
2018-01-14 02:30:40 +0300

    replace

--- resources/js/code.js
+++ resources/js/code.js
@@ -1,9 +1,18 @@
 'use strict';
 window.onload = function mainPreMain() {
+	[].forEach.call(document.querySelectorAll('.logIN') , function(value) {
+		let bg = document.querySelector('.pop-up-meny');
+		value.onclick = function () {
+			bg.style.display = 'block';
+			document.querySelector('.pop-up__bg').onclick = function () {
+				bg.style.display = 'none';
+			};
+		};
+	});
 	document.querySelector('.sidebar__hide-list').onclick = function() {
 		let firstClick = true;
+		let nav = document.querySelector('.nav');
 		return function() {
-			let nav = document.querySelector('.nav');
 			if (firstClick) {
 				let sidebar = document.querySelector('.sidebar__tops');
 				sidebar.appendChild(nav);
