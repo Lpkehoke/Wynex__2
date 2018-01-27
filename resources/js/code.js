8facef4b7cf58122b029640773220f3434eaaaf5
alex <ibymif2000@yandex.ru>
2018-01-27 20:41:57 +0300

    0.02

--- resources/js/code.js
+++ resources/js/code.js
@@ -1,27 +1,21 @@
 'use strict';
 window.onload = function mainPreMain() {
-	[].forEach.call(document.querySelectorAll('.logIN') , function(value) {
+	[].forEach.call(document.querySelectorAll('.logIN') , (value) => {
 		let bg = document.querySelector('.pop-up-meny');
-		value.onclick = function () {
+		value.onclick = () => {
 			bg.style.display = 'block';
-			document.querySelector('.pop-up__bg').onclick = function () {
+			document.querySelector('.pop-up__bg').onclick = () => {
 				bg.style.display = 'none';
 			};
 		};
 	});
 	document.querySelector('.sidebar__hide-list').onclick = function() {
-		let firstClick = true;
-		let nav = document.querySelector('.nav');
+		let nav = document.querySelector('.nav2');
 		return function() {
-			if (firstClick) {
-				let sidebar = document.querySelector('.sidebar__tops');
-				sidebar.appendChild(nav);
-				firstClick = false;
-			}
 			if(nav.style.display === 'flex')
 				nav.style.display = 'none';
 			else
 				nav.style.display = 'flex';
-		}
+		};
 	}();
 };
